807cc93ed6f75c40449a9374b7ed236038427834
gabrielzhen <zhengtingjun@live.cn>
2023-05-06 09:45:24 +0800

    add index page templete

--- MyPythonProject/project/Flask230412/app.py
+++ MyPythonProject/project/Flask230412/app.py
@@ -1,10 +1,26 @@
-from flask import Flask,url_for
+from flask import Flask,url_for,render_template
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 app=Flask(__name__)
 
 @app.route('/',methods=['POST','GET'])
-def hello():
-    return 'welcome to my watchlist!'
+def index():
+    return render_template('index.html',name=name,movies=movies)
+
+
 @app.route('/user/<name>')
 def user_page(name):
     return f'name: {name}'
@@ -20,3 +36,4 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'ztj page'
+
