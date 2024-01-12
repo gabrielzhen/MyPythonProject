051bae56b437e9241061c50f1a4c6194c0dba00d
gabrielzhen <zhengtingjun@live.cn>
2024-01-12 16:48:30 +0800

    2024年1月12日

---
+++ MyPythonProject/project/MicroBlog/app/routes.py
@@ -0,0 +1,23 @@
+from app import app
+from flask import render_template,flash,redirect
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user={'username':'Miguei'}
+    posts=[
+        {'author':{'username':'John'},
+         'body':'beautiful day in portland'},
+        {'author':{'username':'susan'},
+         'body':'the avengers movie was so cool'}
+    ]
+    return render_template('index.html',title='Home',user=user,posts=posts)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested from user{} ,remember_me={}'.format(form.username.data,form.password.data))
+    return redirect('/index')
+    return render_template('login.html',title='Sign In',form=form)
