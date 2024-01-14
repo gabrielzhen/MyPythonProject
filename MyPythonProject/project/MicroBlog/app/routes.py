8a1cd91781b988f14fbc75888c16d575170878fd
gabrielzhen <zhengtingjun@live.cn>
2024-01-14 11:36:32 +0800

    2024年1月14日

--- MyPythonProject/project/MicroBlog/app/routes.py
+++ MyPythonProject/project/MicroBlog/app/routes.py
@@ -19,5 +19,5 @@ def login():
     form=LoginForm()
     if form.validate_on_submit():
         flash('Login requested from user{} ,remember_me={}'.format(form.username.data,form.password.data))
-    return redirect('/index')
+        return redirect('/index')
     return render_template('login.html',title='Sign In',form=form)
