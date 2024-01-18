40b5e17a6f66c3cb71f39271a770604cd85c1470
gabrielzhen <zhengtingjun@live.cn>
2024-01-18 17:30:54 +0800

    2024年1月18日

--- MyPythonProject/project/MicroBlog/app/routes.py
+++ MyPythonProject/project/MicroBlog/app/routes.py
@@ -1,23 +1,53 @@
-from app import app
-from flask import render_template,flash,redirect
-from app.forms import LoginForm
+from app import app,db
+from flask import render_template,flash,redirect,request
+from app.forms import LoginForm,RegistrationForm
+from flask_login import current_user,login_user,logout_user,login_required
+from app.models import User
+import sqlalchemy as sa
+from urllib.parse import urlsplit
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user={'username':'Miguei'}
+    # user={'username':'Miguei'}
     posts=[
         {'author':{'username':'John'},
          'body':'beautiful day in portland'},
         {'author':{'username':'susan'},
          'body':'the avengers movie was so cool'}
     ]
-    return render_template('index.html',title='Home',user=user,posts=posts)
+    return render_template('index.html',title='Home',posts=posts)
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect('/index')
     form=LoginForm()
     if form.validate_on_submit():
-        flash('Login requested from user{} ,remember_me={}'.format(form.username.data,form.password.data))
-        return redirect('/index')
+        user=db.session.scalar(
+            sa.select(User).where(User.username==form.username.data)
+        )
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect('/login')
+        login_user(user,remember=form.remember_me.data)
+        next_page=request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc!='':
+            next_page=('/index')
+        return redirect(next_page)
+        
     return render_template('login.html',title='Sign In',form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/index')
+
+@app.route('/register',method=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect('/index')
+    form=RegistrationForm()
+    
+    
--- MyPythonProject/project/MicroBlog/config.py
+++ MyPythonProject/project/MicroBlog/config.py
@@ -1,3 +1,8 @@
+# 配置文件
 import os
+basedir=os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY') or 'hard code'
+    SQLALCHEMY_DATABASE_URI=os.environ.get('database_url') or \
+    'sqlite:///'+os.path.join(basedir,'app.db')
